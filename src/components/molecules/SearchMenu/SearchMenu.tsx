dba46c57d49b8aba6c36144afa0e6f9386d7c25e
JustNeiz <timur0953017585@gmail.com>
2024-09-08 13:25:25 +0200

    input mobile version fixed

--- src/components/molecules/SearchMenu/SearchMenu.tsx
+++ src/components/molecules/SearchMenu/SearchMenu.tsx
@@ -6,7 +6,7 @@ import { useMediaQuery } from "@mantine/hooks";
 const SearchMenu = () => {
   const isLargeScreen = useMediaQuery("(min-width: 1280px)");
   return (
-    <Flex justify={isLargeScreen ? "flex-end" : 'space-between'} align="center" w={'100%'}>
+    <Flex justify={isLargeScreen ? "flex-end" : 'space-between'} mt={isLargeScreen ? 0 : 10} align="center" w={'100%'}>
       <CurrentCity />
       <SearchAutocomplete />
     </Flex>
